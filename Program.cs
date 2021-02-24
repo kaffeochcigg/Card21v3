a152740f4a236565051ef378d9c32f3c7f59ef68
Oscar Carlsson <Oscar.carlsson@LIVE.COM>
2021-02-24 11:56:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Card21v3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
